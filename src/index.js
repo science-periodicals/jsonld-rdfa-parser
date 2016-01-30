ef1706779763b89229abf3ae195f051ccae00429
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2016-01-29 21:20:16 -0500

    add support for HTML strings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import greenTurtleScript from 'green-turtle';
 import jsdom from 'jsdom';
 import { XMLSerializer } from 'xmldom';
+import isUrl from 'is-url';
 
 const RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
 const RDF_XML_LITERAL = RDF + 'XMLLiteral';
@@ -12,95 +13,129 @@ const XSD_STRING = 'http://www.w3.org/2001/XMLSchema#string';
 const ELEMENT_NODE = 1;
 const TEXT_NODE = 3;
 
-export default function jsonldRdfaParser(htmlPath, callback) {
-  jsdom.env({
-    file: htmlPath,
-    scripts: [greenTurtleScript],
-    done: function(err, window) {
-      if (err) console.error(err);
+/**
+ * @param data - a filePath, HTML string or URL
+ */
+export default function jsonldRdfaParser(data, callback) {
 
-      var data = window.document.data.graph;
+  let config = {
+    scripts: [greenTurtleScript]
+  };
 
-      var dataset = {};
-      dataset['@default'] = [];
+  if (typeof data !== 'string') {
+    return callback(new Error('data must be a file path, HTML string or URL'));
+  }
 
-      // what follows is mostly taken from the jsonld.js lib but updated to the lattest green-turtle API
+  if (isUrl(data)) {
+    config.url = data;
+  } else if (/<[a-z][\s\S]*>/i.test(data)) {
+    config.url = 'file:///'; // without that, green turtle will throw: `Cannot resolve uri against non-generic URI: about:blank`
+    config.html = data;
+  } else {
+    config.file = data;
+  }
 
-      var subjects = data.subjects;
-      for (var subject in subjects) {
-        var predicates = subjects[subject].predicates;
+  config.done = function(err, window) {
+    if (err) return callback(err);
 
-        for (var predicate in predicates) {
-          // iterate over objects
-          var objects = predicates[predicate].objects;
-          for (var oi = 0; oi < objects.length; ++oi) {
-            var object = objects[oi];
+    let dataset, processingError;
+    try {
+      window.GreenTurtle.attach(window.document);
+      dataset = processGraph(window.document.data.graph)
+    } catch (e) {
+      processingError = e;
+    }
 
-            // create RDF triple
-            var triple = {};
+    callback(processingError, dataset);
+  };
 
-            // add subject
-            if (subject.indexOf('_:') === 0) {
-              triple.subject = {type: 'blank node', value: subject};
-            } else {
-              triple.subject = {type: 'IRI', value: subject};
+  jsdom.env(config);
+};
+
+
+/**
+ * This function is mostly taken from the jsonld.js lib but updated to
+ * the lattest green-turtle API
+ */
+
+function processGraph(data) {
+
+  var dataset = {};
+  dataset['@default'] = [];
+
+  var subjects = data.subjects;
+  for (var subject in subjects) {
+    var predicates = subjects[subject].predicates;
+
+    for (var predicate in predicates) {
+      // iterate over objects
+      var objects = predicates[predicate].objects;
+      for (var oi = 0; oi < objects.length; ++oi) {
+        var object = objects[oi];
+
+        // create RDF triple
+        var triple = {};
+
+        // add subject
+        if (subject.indexOf('_:') === 0) {
+          triple.subject = {type: 'blank node', value: subject};
+        } else {
+          triple.subject = {type: 'IRI', value: subject};
+        }
+
+        // add predicate
+        if (predicate.indexOf('_:') === 0) {
+          triple.predicate = {type: 'blank node', value: predicate};
+        } else {
+          triple.predicate = {type: 'IRI', value: predicate};
+        }
+
+        // serialize XML literal
+        var value = object.value;
+        if (object.type === RDF_XML_LITERAL) {
+          // initialize XMLSerializer
+          var serializer = new XMLSerializer();
+          value = '';
+          for (var x = 0; x < object.value.length; x++) {
+            if (object.value[x].nodeType === ELEMENT_NODE) {
+              value += serializer.serializeToString(object.value[x]);
+            } else if(object.value[x].nodeType === TEXT_NODE) {
+              value += object.value[x].nodeValue;
             }
-
-            // add predicate
-            if (predicate.indexOf('_:') === 0) {
-              triple.predicate = {type: 'blank node', value: predicate};
-            } else {
-              triple.predicate = {type: 'IRI', value: predicate};
-            }
-
-            // serialize XML literal
-            var value = object.value;
-            if (object.type === RDF_XML_LITERAL) {
-              // initialize XMLSerializer
-              var serializer = new XMLSerializer();
-              value = '';
-              for (var x = 0; x < object.value.length; x++) {
-                if (object.value[x].nodeType === ELEMENT_NODE) {
-                  value += serializer.serializeToString(object.value[x]);
-                } else if(object.value[x].nodeType === TEXT_NODE) {
-                  value += object.value[x].nodeValue;
-                }
-              }
-            }
-
-            // add object
-            triple.object = {};
-
-            // object is an IRI
-            if (object.type === RDF_OBJECT) {
-              if (object.value.indexOf('_:') === 0) {
-                triple.object.type = 'blank node';
-              } else {
-                triple.object.type = 'IRI';
-              }
-            } else {
-              // object is a literal
-              triple.object.type = 'literal';
-              if (object.type === RDF_PLAIN_LITERAL) {
-                if (object.language) {
-                  triple.object.datatype = RDF_LANGSTRING;
-                  triple.object.language = object.language;
-                } else {
-                  triple.object.datatype = XSD_STRING;
-                }
-              } else {
-                triple.object.datatype = object.type;
-              }
-            }
-            triple.object.value = value;
-
-            // add triple to dataset in default graph
-            dataset['@default'].push(triple);
           }
         }
-      }
 
-      callback(null, dataset);
+        // add object
+        triple.object = {};
+
+        // object is an IRI
+        if (object.type === RDF_OBJECT) {
+          if (object.value.indexOf('_:') === 0) {
+            triple.object.type = 'blank node';
+          } else {
+            triple.object.type = 'IRI';
+          }
+        } else {
+          // object is a literal
+          triple.object.type = 'literal';
+          if (object.type === RDF_PLAIN_LITERAL) {
+            if (object.language) {
+              triple.object.datatype = RDF_LANGSTRING;
+              triple.object.language = object.language;
+            } else {
+              triple.object.datatype = XSD_STRING;
+            }
+          } else {
+            triple.object.datatype = object.type;
+          }
+        }
+        triple.object.value = value;
+
+        // add triple to dataset in default graph
+        dataset['@default'].push(triple);
+      }
     }
-  });
+  }
+
+  return dataset;
 };
--- test/test.js
+++ test/test.js
@@ -5,10 +5,74 @@ import path from 'path';
 import mocha from 'mocha';
 import jsonld from 'jsonld';
 import jsonldRdfaParser from '../src';
+import { jsdom } from 'jsdom';
 
 const htmlPath = path.join(path.dirname(__filename), 'fixtures', 'test.html');
 
-// var html = fs.readFileSync(htmlPath, {encoding: 'utf8'});
+const expected = {
+  '@id': 'http://example.com',
+  '@type': 'ScholarlyArticle',
+  author: {
+    '@id': '_:b0',
+    '@type': 'sa:ContributorRole',
+    roleAffiliation: {
+      '@id': 'https://science.ai/',
+      '@type': 'Corporation',
+      name: 'science.ai'
+    },
+    roleContactPoint: {
+      '@id': '_:b1',
+      '@type': 'ContactPoint',
+      email: { '@id': 'mailto:robin@berjon.com' }
+    },
+    author:
+    {
+      '@id': 'http://berjon.com/',
+      '@type': 'Person',
+      familyName: 'Berjon',
+      givenName: 'Robin'
+    }
+  },
+  contributor: {
+    '@id': '_:b2',
+    '@type': 'sa:ContributorRole',
+    roleAffiliation:  {
+      '@id': 'https://science.ai/',
+      '@type': 'Corporation',
+      name: 'science.ai'
+    },
+    contributor: {
+      '@id': 'https://github.com/sballesteros',
+      '@type': 'Person',
+      familyName: 'Ballesteros',
+      givenName: 'Sebastien'
+    }
+  }
+};
+
+const context = {
+  "@context": {
+    "@vocab": "http://schema.org/",
+    "sa": "http://ns.science.ai#",
+    "roleAffiliation": {
+      "@id": "sa:roleAffiliation",
+      "@type": "@id"
+    },
+    "roleContactPoint": {
+      "@id": "sa:roleContactPoint",
+      "@type": "@id"
+    },
+    "author": {
+      "@type": "@id"
+    },
+    "contributor": {
+      "@type": "@id"
+    }
+  },
+  "@embed": "@always",
+  "@type": "ScholarlyArticle"
+};
+
 
 describe('jsonld-rdfa-parser', function() {
 
@@ -16,78 +80,36 @@ describe('jsonld-rdfa-parser', function() {
     jsonld.registerRDFParser('html', jsonldRdfaParser);
   });
 
-  it('should parse RDFa', function(done) {
+  it('should parse RDFa from a file path ', function(done) {
     jsonld.fromRDF(htmlPath, {format: 'html'}, function(err, data) {
-      if (err) console.error(err);
-      jsonld.frame(data, {
-        "@context": {
-          "@vocab": "http://schema.org/",
-          "sa": "http://ns.science.ai#",
-          "roleAffiliation": {
-            "@id": "sa:roleAffiliation",
-            "@type": "@id"
-          },
-          "roleContactPoint": {
-            "@id": "sa:roleContactPoint",
-            "@type": "@id"
-          },
-          "author": {
-            "@type": "@id"
-          },
-          "contributor": {
-            "@type": "@id"
-          }
-        },
-        "@embed": "@always",
-        "@type": "ScholarlyArticle"
-      }, function(err, data) {
-
-        let expected = {
-          '@id': 'http://example.com',
-          '@type': 'ScholarlyArticle',
-          author: {
-            '@id': '_:b0',
-            '@type': 'sa:ContributorRole',
-            roleAffiliation: {
-              '@id': 'https://science.ai/',
-              '@type': 'Corporation',
-              name: 'science.ai'
-            },
-            roleContactPoint: {
-              '@id': '_:b1',
-              '@type': 'ContactPoint',
-              email: { '@id': 'mailto:robin@berjon.com' }
-            },
-            author:
-            {
-              '@id': 'http://berjon.com/',
-              '@type': 'Person',
-              familyName: 'Berjon',
-              givenName: 'Robin'
-            }
-          },
-          contributor: {
-            '@id': '_:b2',
-            '@type': 'sa:ContributorRole',
-            roleAffiliation:  {
-              '@id': 'https://science.ai/',
-              '@type': 'Corporation',
-              name: 'science.ai'
-            },
-            contributor: {
-              '@id': 'https://github.com/sballesteros',
-              '@type': 'Person',
-              familyName: 'Ballesteros',
-              givenName: 'Sebastien'
-            }
-          }
-        };
-
+      jsonld.frame(data, context, function(err, data) {
         assert.deepEqual(data['@graph'][0], expected);
         done();
       });
     });
   });
 
+  it('should parse RDFa from a string of HTML ', function(done) {
+    fs.readFile(htmlPath, {encoding: 'utf8'}, function(err, html) {
+      jsonld.fromRDF(html, {format: 'html'}, function(err, data) {
+        jsonld.frame(data, context, function(err, data) {
+          assert.deepEqual(data['@graph'][0], expected);
+          done();
+        });
+      });
+    });
+  });
+
+//  it('should parse RDFa from a DOM node', function(done) {
+//    fs.readFile(htmlPath, {encoding: 'utf8'}, function(err, html) {
+//      jsonld.fromRDF(html, {format: 'html'}, function(err, data) {
+//        console.log(data);
+//        jsonld.frame(data, context, function(err, data) {
+//          assert.deepEqual(data['@graph'][0], expected);
+//          done();
+//        });
+//      });
+//    });
+//  });
 
 });
